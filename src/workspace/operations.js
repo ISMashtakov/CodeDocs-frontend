b87d19805f489f5f69d2cd2312aa3082fbd5ef89
imashtak <mashtakovigor@mail.ru>
2021-04-13 15:12:14 +0300

    for save

--- src/workspace/operations.js
+++ src/workspace/operations.js
@@ -2,23 +2,88 @@ import {diff_match_patch} from '../packages/text_differents';
 
 
 class Operation{
-
+    getMessage(){
+    }
+    
+    changeByOperation(oper){
+        if (!(oper instanceof Operation)){
+            throw new Error("illegal argument")
+        }
+        return this
+    }
 }
 
 class Insert extends Operation{
+    constructor(pos, text){
+        super()
+        this.pos = pos
+        this.text = text
+    }
 
+    getMessage(){
+        return{
+            type: "insert",
+            position: this.pos,
+            text: this.text
+        }
+    }
+
+    changeByOperation(oper){
+        super.changeByOperation(oper);
+
+        if (oper instanceof Insert){
+            if(this.pos <= oper.pos){
+                return new Insert(this.pos, this.text)
+            }
+            else {
+                return new Insert(this.pos + oper.text.length, this.text)
+            }
+        }
+        
+
+    }
 }
 
 class Delete extends Operation{
+    constructor(pos, text){
+        super()
+        this.pos = pos
+        this.text = text
+    }
 
+    getMessage(){
+        return{
+            type: "delete",
+            position: this.pos,
+            text: this.text
+        }
+    }
 }
 
 class Neutral extends Operation{
-
+    getMessage(){
+        return{
+            type: "neutral"
+        }
+    }
 }
 
-export function getOperation(oldText, newText) {
-    const diff = new diff_match_patch()    
-    var a = diff.diff_main(oldText, newText)
-    console.log(a)
+export function getOperations(oldText, newText) {
+    const diff = (new diff_match_patch()).diff_main(oldText, newText)
+    const operations = []
+    var pos = 0;
+    diff.forEach(item => {
+        switch(item[0]){
+            case 1:
+                const insertOp = new Insert(pos, item[1])
+                operations.push(insertOp)
+                break
+            case -1:
+                const deleteOp = new Delete(pos, item[1])
+                operations.push(deleteOp)
+                break
+        }
+        pos += item[1].length
+    });
+    return operations
 }
